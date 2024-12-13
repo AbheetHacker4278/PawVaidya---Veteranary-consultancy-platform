f3acff0280a75c7d223b02b91882a3da2d70fa28
Abheet seth <114290748+AbheetHacker4278@users.noreply.github.com>
2024-12-13 23:48:23 +0530

    small bugs server.js (#31)
    * Bugs Fixes and Security Updates
    
    * Small Bug
    
    * Minor Bugs

--- PawVaidya/backend/server.js
+++ PawVaidya/backend/server.js
@@ -16,7 +16,7 @@ connectCloudnairy() //
 
 
 //middleware
-const allowedorigins = ['http://localhost:5173' , "*"]
+const allowedorigins = ['http://localhost:5173' , "*" , 'http://localhost:5174']
 app.use(express.json())
 app.use(cors({ origin: allowedorigins , credentials: true}));
 app.use(cookieParser())
@@ -25,7 +25,7 @@ app.use(cookieParser())
 try {
     app.use('/api/admin', adminRouter);
   } catch (error) {
-    console.error("Failed to use adminRouter:", error.message);
+    console.error("failed to use adminRouter:", error.message);
 }
 try {
     app.use('/api/doctor', doctorrouter);
